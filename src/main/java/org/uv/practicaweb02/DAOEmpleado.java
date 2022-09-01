5e2d8129d160b2fae8dbb838024ff9603ff4e404
Gisellemb03 <gisellemendozab03@gmail.com>
2022-08-31 19:46:04 -0500

    daoempleado

--- src/main/java/org/uv/practicaweb02/DAOEmpleado.java
+++ src/main/java/org/uv/practicaweb02/DAOEmpleado.java
@@ -4,7 +4,12 @@
  */
 package org.uv.practicaweb02;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -23,22 +28,60 @@ public class DAOEmpleado implements IDAOGeneral<Empleado, Integer>{
 
     @Override
     public boolean modificar(Empleado pojo, Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ConexionDB con = ConexionDB.getInstance();
+        String sql = "UPDATE empleado SET nombre= '"+pojo.getNombre()+"', direccion="
+                + "'"+pojo.getDireccion()+"', telefono='"+pojo.getTelefono()+"' "
+                + "WHERE clave = '"+pojo.getClave()+"'";
+        return con.execute(sql);
     }
 
     @Override
     public boolean eliminar(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ConexionDB con = ConexionDB.getInstance();
+        String sql= "DELETE FROM empleado WHERE clave = '"+id+"'";
+        return con.execute(sql);
     }
 
     @Override
     public Empleado buscarById(Integer id) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ConexionDB con = ConexionDB.getInstance();
+        try {
+            String sql ="Select * from empleado where clave='"+id+"'";
+            Empleado empleado = new Empleado();
+            ResultSet reg=con.query(sql);
+            if(reg.next()){
+                empleado.setClave(reg.getInt("clave"));
+                empleado.setNombre(reg.getString("nombre"));
+                empleado.setDireccion(reg.getString("direccion"));
+                empleado.setTelefono(reg.getString("telefono"));
+                return empleado;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
 
     @Override
     public List<Empleado> buscarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        ConexionDB con = ConexionDB.getInstance();
+        try {
+            String sql ="Select * from empleado";
+            List<Empleado> lstEmpleado= new ArrayList<>();
+            ResultSet reg=con.query(sql);
+            while(reg.next()){
+                Empleado empleado = new Empleado();
+                empleado.setClave(reg.getInt("clave"));
+                empleado.setNombre(reg.getString("nombre"));
+                empleado.setDireccion(reg.getString("direccion"));
+                empleado.setTelefono(reg.getString("telefono"));
+                lstEmpleado.add(empleado);
+            }
+            return lstEmpleado;
+        } catch (SQLException ex) {
+            Logger.getLogger(DAOEmpleado.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
     }
     
 }
